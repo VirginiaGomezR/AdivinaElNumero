8e7c9dc1e5ed537282747c99cb2ca166aedb4740
VirginiaGomezR <virginiagomezr@gmail.com>
2020-06-30 14:16:54 +0200

    exercise fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict';
-let inputNumber = document.querySelector('.inputNumber');
-let text = document.querySelector('.text');
+const inputNumber = document.querySelector('.js-inputNumber');
+const text = document.querySelector('.js-text');
+const tries = document.querySelector('.js-tries');
+
 const button = document.querySelector('.js-clickButton');
 
 //Función que genera número aleatorio
@@ -9,18 +11,41 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 const randomNumber = getRandomNumber(max);
-console.log(randomNumber);
+console.log('Mi número aleatorio es:', randomNumber);
 
 //Funciones
 
-function validationNumber(randomNumber) {
-  if (randomNumber > inputNumber.value) {
+function handleSubmitButton(ev) {
+  ev.preventDefault();
+  //prevenir el evento del formulario ya que es de tipo "submit" ev.preventDefault();
+  validationNumber();
+  updateCounter();
+}
+
+function validationNumber() {
+  console.log(inputNumber.value, randomNumber, inputNumber === randomNumber);
+  //transformamos a número para que la equivalencia sea númerica para ambos valores
+  let numberValue = parseInt(inputNumber.value);
+  if (numberValue <= 1 || numberValue >= 100) {
+    attemps++;
+    text.innerHTML = 'El número debe estar entre 1 y 100';
+  } else if (randomNumber > numberValue) {
+    attemps++;
     text.innerHTML = 'Demasiado bajo';
-  } else if (randomNumber == inputNumber.value) {
+  } else if (randomNumber === numberValue) {
     text.innerHTML = 'Has ganado campeona!';
   } else {
+    attemps++;
     text.innerHTML = 'Demasiado alto';
   }
 }
 
-button.addEventListener('click', () => validationNumber(randomNumber));
+//Intentos
+let attemps = 0;
+function updateCounter() {
+  attemps++; //equivale a (attemps=attemps +1)
+  tries.innerHTML = attemps;
+  console.log('Incrementando', attemps);
+}
+
+button.addEventListener('click', handleSubmitButton);
